24b8748dfaf46e66f1aafab57a3ff0cb76cf8449
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-03 16:46:51 +0200

    test nomi in pagina

--- src/components/Pokemon/PokemonList.vue
+++ src/components/Pokemon/PokemonList.vue
@@ -1,12 +1,23 @@
 <script>
+import axios from 'axios';
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 export default {
-
+  data() {
+    return {
+      pokemons: []
+    }
+  },
+  created() {
+    axios.get(endpoint).then(res => {
+      this.pokemons = res.data.docs;
+    })
+  }
 }
 </script>
 
 <template>
   <div id="pokemons">
-
+    <h4 v-for="pokemon in pokemons" :key="pokemon._id">{{ pokemon.name }}</h4>
   </div>
 </template>
 
