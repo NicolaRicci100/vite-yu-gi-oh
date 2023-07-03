4ad3d8048a4c9fedd7d79f55a746696c84321f3e
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-03 17:12:43 +0200

    passo alla lista le informazioni delle card

---
+++ src/components/Pokemon/PokemonCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  props: {
+    name: String,
+    number: Number,
+    image: String,
+    type: String
+  }
+}
+</script>
+
+<template></template>
+
+<style lang="scss" scoped></style>
--- src/components/Pokemon/PokemonList.vue
+++ src/components/Pokemon/PokemonList.vue
@@ -1,7 +1,9 @@
 <script>
 import axios from 'axios';
+import PokemonCard from './PokemonCard.vue';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
 export default {
+  components: { PokemonCard },
   data() {
     return {
       pokemons: []
@@ -17,7 +19,12 @@ export default {
 
 <template>
   <div id="pokemons">
-    <h4 v-for="pokemon in pokemons" :key="pokemon._id">{{ pokemon.name }}</h4>
+    <div class="row row-cols-5">
+      <div class="col" v-for="pokemon in pokemons" :key="pokemon._id">
+        <PokemonCard :name="pokemon.name" :type="pokemon.type1" :image="pokemon.imageUrl" :number="pokemon.number" />
+      </div>
+    </div>
+
   </div>
 </template>
 
